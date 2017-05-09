8e41675e4da2de0d0020cb686fee1bcd5e6f9ef5
Marcus Eduardo <marcusedu7@gmail.com>
2017-05-08 21:22:11 -0300

    Alguns testes automatizados de copiar/mover arquivo

--- app/src/androidTest/java/br/com/powerapps/powerimagecompress/PAImgCprsTest.java
+++ app/src/androidTest/java/br/com/powerapps/powerimagecompress/PAImgCprsTest.java
@@ -1,8 +1,7 @@
 package br.com.powerapps.powerimagecompress;
 
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.test.InstrumentationRegistry;
+import android.os.Environment;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
@@ -12,6 +11,7 @@ import org.junit.runner.RunWith;
 import java.io.File;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -22,12 +22,14 @@ import static org.junit.Assert.assertTrue;
  */
 @RunWith(AndroidJUnit4.class)
 public class PAImgCprsTest {
+    private PAImgCprs compress;
+    private File arquivoBase;
+
     @Before
     public void setUp() throws Exception {
-        compress = PowerImageCompress.doArquivo("/sdcard/img.jpg");
+        arquivoBase = new File("/sdcard/img.jpg");
+        compress = PowerImageCompress.doArquivo(arquivoBase);
     }
-
-    private PAImgCprs compress;
 
     @Test
     public void deveLancarUmaExcepcaoNull() throws Exception {
@@ -77,5 +79,34 @@ public class PAImgCprsTest {
                 .manterProporcao(false)
                 .copiarPara(novo);
         assertTrue(novo.equals(novoRecebido));
+    }
+
+    @Test
+    public void deveCopiarCorretamenteOArquivoParaODiretorioInformado() throws Exception {
+        File file = new File(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/test/");
+        File resultado = compress.copiarPara(file);
+        assertEquals("img.jpg", resultado.getName());
+    }
+
+    @Test
+    public void deveCopiarCorretamenteOArquivoParaNovoArquivo() throws Exception {
+        File arquivoCopiado = compress.copiarPara(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/test/copiaImg.jpg");
+        assertNotEquals(arquivoBase.getName(), arquivoCopiado.getName());
+    }
+
+    @Test
+    public void deveMoverCorretamenteOArquivoParaODiretorioInformado() throws Exception {
+        File movido = compress.moverPara(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/test/");
+        assertTrue(movido.length() > 0);
+        assertFalse(arquivoBase.exists());
+        assertEquals(arquivoBase.getName(), movido.getName());
+    }
+
+    @Test
+    public void deveMoverCorretamenteOArquivoParaNovoArquivo() throws Exception {
+        File movido = compress.moverPara(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/test/movidoImg.jpg");
+        assertTrue(movido.length() > 0);
+        assertFalse(arquivoBase.exists());
+        assertNotEquals(arquivoBase.getName(), movido.getName());
     }
 }
